08559c62441d06e81cc4d59b88f9092f8aba65c6
KerelusMoner <kerelusmoner@gmail.com>
2022-07-04 00:14:30 +0200

    Initial Commit for lab 6

---
+++ assets/js/main.js
@@ -0,0 +1,56 @@
+
+(function () {
+  "use strict";
+
+  /**
+   * Porfolio isotope and filter
+   */
+  window.addEventListener("load", () => {
+    let portfolioContainer = document.querySelector(".portfolio-container");
+    if (portfolioContainer) {
+      let portfolioIsotope = new Isotope(portfolioContainer, {
+        itemSelector: ".portfolio-item",
+        layoutMode: "fitRows",
+      });
+
+      let portfolioFilters = document.querySelector("#portfolio-flters li", true);
+
+      on(
+        "click",
+        "#portfolio-flters li",
+        function (e) {
+          e.preventDefault();
+          portfolioFilters.forEach(function (el) {
+            el.classList.remove("filter-active");
+          });
+          this.classList.add("filter-active");
+
+          portfolioIsotope.arrange({
+            filter: this.getAttribute("data-filter"),
+          });
+          aos_init();
+        },
+        true
+      );
+    }
+  });
+
+
+  const portfolioLightbox = GLightbox({
+    selector: ".portfokio-lightbox",
+  });
+
+ 
+  new Swiper(".portfolio-details-slider", {
+    speed: 400,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+  });
+})();
